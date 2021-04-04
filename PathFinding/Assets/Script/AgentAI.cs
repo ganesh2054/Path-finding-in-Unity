4fe14dcabc1823dd74cb7cb257fb1f0a65bb189c
lme ganesh <lmeganesh123@gmail.com>
2021-04-04 10:35:20 +0545

    initial commit

---
+++ PathFinding/Assets/Script/AgentAI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.AI;
+
+public class AgentAI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    Transform destination;
+    NavMeshAgent agent;
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        if(agent==null)
+        {
+            Debug.Log("error");
+        }
+        else
+        {
+            SetDestination();
+        }
+    }
+
+    // Update is called once per frame
+  public void SetDestination()
+    {
+        if(destination!=null)
+        {
+            Vector3 target = destination.transform.position;
+            agent.SetDestination(target);
+        }
+    }
+}
